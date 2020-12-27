74f16b573a23f08609674e6878f9b87bd0ee6ddc
Vladimir Kovalenko <sekach_79@mail.ru>
2020-12-27 19:45:21 +1000

    first commit data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,27 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+let S = amount - contribution; 
+console.log(S);
+
+let n = date;
+console.log(n);
+let P = (percent*100) / 12;
+let monthlyPayment = S*(P+P/(((1+P)^n)-1)).toFixed(0);
+let totalAmount = (monthlyPayment * n).toFixed(2);
+console.log(totalAmount);
+     return totalAmount;
 }
+
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = (`Привет, мир! Меня зовут ${name}.`);
+    if(name === ""){
+        name === 'Аноним';
+    } else {
+
+    }
+    
+     return greeting;
 }
