0f9fc57ba94af9566183f8c089ed62ca8c5f0e83
Vladimir Kovalenko <sekach_79@mail.ru>
2020-12-27 12:37:52 +1000

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -8,20 +8,28 @@
 function averageMark(a, g, p) {
 	
     // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let averageResult = (algebra + geography + physics) / 3;
+	return averageResult;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}.`
+	return message;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
+    return result;
 }
