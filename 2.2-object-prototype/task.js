df02f457b8ae6335746d7e82a492534f4e12d86d
Vladimir Kovalenko <sekach_79@mail.ru>
2021-01-29 22:20:50 +1000

    first commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,9 +1,24 @@
 //String.prototype.isPalindrome - для задачи №1
-
+String.prototype.isPalindrome = function () {
+    let normalized = this.toLowerCase().match(/[а-я]/gi).reverse();
+    return normalized.join('') === normalized.reverse().join('');
+    }
+    
+   // Задание №2
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if(marks.length === 0){
+        return 0;
+    } else {
+        let sum = 0;
+        for(let i = 0; i < marks.length; i++){
+            sum += marks[i];
+        }
+        let averageMarks = sum / marks.length;
+        let roundedAverage = Math.round(averageMarks);
+        return roundedAverage;
+    }
 }
+
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
